39417391d90f92805e308494c80436b7a27d3417
Chris Hassan <chriskkhassan@gmail.com>
2019-03-05 21:22:54 +0000

    Feature: fizzbuzz(6) returns fizz

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,3 +1,5 @@
 def fizzbuzz(number):
     if number == 3:
         return 'fizz'
+    elif number == 6:
+        return 'fizz'
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -3,10 +3,16 @@ import unittest
 from src.fizzbuzz import fizzbuzz
 
 class TestFizzbuzz(unittest.TestCase):
-    def test_fizzbuzz_returns_number(self):
-        """ Fizzbuzz returns 3 """
+    def test_fizzbuzz_3_returns_fizz(self):
+        """ Fizzbuzz(3) returns fizz """
 
         result = fizzbuzz(3)
+        self.assertEqual(result, 'fizz')
+
+    def test_fizzbuzz_returns_number(self):
+        """ Fizzbuzz(6) returns fizz """
+
+        result = fizzbuzz(6)
         self.assertEqual(result, 'fizz')
 
 if __name__ == '__main__':
