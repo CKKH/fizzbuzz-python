0f80406c1aefa15cfacb33638f27ab27f3d7793f
Chris Hassan <chriskkhassan@gmail.com>
2019-03-05 21:28:18 +0000

    Feature: fizzbuzz(10) returns buzz

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -9,11 +9,17 @@ class TestFizzbuzz(unittest.TestCase):
         result = fizzbuzz(3)
         self.assertEqual(result, 'fizz')
 
-    def test_fizzbuzz_6_returns_number(self):
+    def test_fizzbuzz_6_returns_fizz(self):
         """ Fizzbuzz(6) returns fizz """
 
         result = fizzbuzz(6)
         self.assertEqual(result, 'fizz')
+
+    def test_fizzbuzz_10_returns_buzz(self):
+        """ Fizzbuzz(10) retusn 'buzz' """
+
+        result = fizzbuzz(10)
+        self.assertEqual(result, 'buzz')
 
 if __name__ == '__main__':
     unittest.main()
