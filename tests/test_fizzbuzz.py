0e8f2b0a4e3357f4852da20057b811839d31397b
Chris Hassan <chriskkhassan@gmail.com>
2019-03-05 21:35:51 +0000

    Refactor: removed unncessary tests

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -15,22 +15,10 @@ class TestFizzbuzz(unittest.TestCase):
         result = fizzbuzz(3)
         self.assertEqual(result, 'fizz')
 
-    def test_fizzbuzz_6_returns_fizz(self):
-        """ Fizzbuzz(6) returns fizz """
-
-        result = fizzbuzz(6)
-        self.assertEqual(result, 'fizz')
-
-    def test_fizzbuzz_10_returns_buzz(self):
-        """ Fizzbuzz(10) return 'buzz' """
-
-        result = fizzbuzz(10)
-        self.assertEqual(result, 'buzz')
-
     def test_fizzbuzz_5_returns_buzz(self):
         """ Fizzbuzz(5) return 'buzz' """
 
-        result = fizzbuzz(10)
+        result = fizzbuzz(5)
         self.assertEqual(result, 'buzz')
 
 if __name__ == '__main__':
