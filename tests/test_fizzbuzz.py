233757353f7c1383f2c13335f1d8a92bba557e22
Chris Hassan <chriskkhassan@gmail.com>
2019-03-08 15:59:45 +0000

    Refactor: called fizzbuzz directly in test assertions

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -5,26 +5,22 @@ class TestFizzbuzz(unittest.TestCase):
     def test_fizzbuzz_1_returns_buzz(self):
         """ Fizzbuzz(1) return 'buzz' """
 
-        result = fizzbuzz(1)
-        self.assertEqual(result, 1)
+        self.assertEqual(fizzbuzz(1), 1)
 
     def test_fizzbuzz_3_returns_fizz(self):
         """ Fizzbuzz(3) returns fizz """
 
-        result = fizzbuzz(3)
-        self.assertEqual(result, 'fizz')
+        self.assertEqual(fizzbuzz(3), 'fizz')
 
     def test_fizzbuzz_5_returns_buzz(self):
         """ Fizzbuzz(5) return 'buzz' """
 
-        result = fizzbuzz(5)
-        self.assertEqual(result, 'buzz')
+        self.assertEqual(fizzbuzz(5), 'buzz')
 
     def test_fizzbuzz_15_returns_fizzbuzz(self):
         """ Fizzbuzz(15) returns 'fizzbuzz' """
 
-        result = fizzbuzz(15)
-        self.assertEqual(result, 'fizzbuzz')
+        self.assertEqual(fizzbuzz(15), 'fizzbuzz')
 
 if __name__ == '__main__':
     unittest.main()
